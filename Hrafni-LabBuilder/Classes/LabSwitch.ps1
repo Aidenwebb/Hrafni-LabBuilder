d0b94363fbae3ba2ee3462983cec473dec76d679
Aiden Arnkels-Webb <aiden.webb@gmail.com>
2020-09-03 01:13:21 +0100

    Moved HyperVSwitch creation logic to class

--- Hrafni-LabBuilder/Classes/LabSwitch.ps1
+++ Hrafni-LabBuilder/Classes/LabSwitch.ps1
@@ -20,5 +20,24 @@ class LabSwitch
         $this.BindAdapter = $BindAdapter
     }
     
+    CreateHyperVSwitch() {
+        Write-Verbose "Initialising Switch: $($this.Name)"
+        Write-Verbose "Testing if LabSwitch already exists in Hyper-V: $($this.Name)"
+        # Don't create a duplicate switch if one already has the same name
+        if ((Get-VMSwitch | Where-Object -Property Name -EQ $this.Name).count -eq 0) {
+            Write-Verbose "Switch does not exist: $($this.Name)"
+            Write-Verbose "Creating Switch: $($this.Name)"
+            if ($this.Type -eq "External")
+            {
+                New-VMSwitch -Name $this.Name -NetAdapterName $this.BindAdapter.Name -AllowManagementOS $true
+            }
+            else 
+            {
+                New-VMSwitch -Name $this.Name -SwitchType $this.Type
+            }
+        } else {
+            Write-Verbose "Switch already exist and will not be created: $($this.Name)"
+        }
+    }
     
 }
--- Hrafni-LabBuilder/Public/Initialize-LabSwitch.ps1
+++ Hrafni-LabBuilder/Public/Initialize-LabSwitch.ps1
@@ -12,23 +12,7 @@ function Initialize-LabSwitch {
         Write-Verbose "Looping through LabSwitch configurations"
         foreach ($Switch in $Switches)
         {
-            Write-Verbose "Initialising Switch: $($Switch.Name)"
-            Write-Verbose "Testing if LabSwitch already exists in Hyper-V: $($Switch.Name)"
-            # Don't create a duplicate switch if one already has the same name
-            if ((Get-VMSwitch | Where-Object -Property Name -EQ $Switch.Name).count -eq 0) {
-                Write-Verbose "Switch does not exist: $($Switch.Name)"
-                Write-Verbose "Creating Switch: $($Switch.Name)"
-                if ($Switch.Type -eq "External")
-                {
-                    New-VMSwitch -Name $Switch.Name -NetAdapterName $Switch.BindAdapter.Name -AllowManagementOS $true
-                }
-                else 
-                {
-                    New-VMSwitch -Name $Switch.Name -SwitchType $Switch.Type
-                }
-            } else {
-                Write-Verbose "Switch already exist and will not be created: $($Switch.Name)"
-            }
+            $Switch.CreateHyperVSwitch()
             
         }
     }
