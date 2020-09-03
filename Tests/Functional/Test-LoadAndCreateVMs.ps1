017e6055bcd1a32a758938fe0adb0edcb347763c
Aiden Arnkels-Webb <aiden.webb@gmail.com>
2020-09-03 02:31:25 +0100

    Fixed typo and added initialise vm's to functional test

--- Tests/Functional/Test-LoadAndCreateVMs.ps1
+++ Tests/Functional/Test-LoadAndCreateVMs.ps1
@@ -3,4 +3,4 @@ Import-Module Hrafni-LabBuilder
 $ConfigPath = ".\Hrafni-LabBuilder\configs\labconfig.xml"
 $LabConfig = Get-LabConfig -ConfigPath $ConfigPath -Verbose
 $VMs = Get-LabVMConfig -LabConfig $LabConfig -Verbose
-#$InitializedVMs = Initialize-LabSwitch -Switches $Switches -Verbose
+$InitializedVMs = Initialize-LabVM -VMs $VMs -Verbose
