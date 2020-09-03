09134e75a15529a07ee2987e561c83e4eaec3c0b
Aiden Arnkels-Webb <aiden.webb@gmail.com>
2020-09-03 20:27:18 +0100

    Wrote function to create VHDX from WIM
    For templating engine

---
+++ Hrafni-LabBuilder/Public/New-VHDXsFromWIM.ps1
@@ -0,0 +1,64 @@
+function New-VHDXsFromWIM {
+    [CmdletBinding()]
+    param (
+        [string]$WIMPath,
+        [string]$OutputDirectory
+    )
+    
+    begin {
+
+        # Name Sanitiser Pattern
+        $pattern = '[^a-zA-Z0-9\-]'
+
+        # Server naming pattern for Conver-WindowsImage
+
+        # Test Paths
+        If (-not(Test-Path $WIMPath))
+        {
+            throw "ISO not found"
+        }
+        $WIM = Get-Item $WIMPath
+
+        If (-not(Test-Path $OutputDirectory))
+        {
+            throw "OutputDirectory not found"
+        }
+        $OutputDirectory = (Get-Item $OutputDirectory).FullName
+    
+        $Images = Get-WindowsImage -ImagePath $WIM.FullName
+
+    }
+    
+    process {
+        foreach ($Image in $Images) {
+            Write-Verbose "ImageName: $($image.ImageName)"
+
+            switch ($Image.ImageName) {
+                "Windows Server 2019 Standard" { $Edition = "ServerStandardCore" }
+                "Windows Server 2019 Standard (Desktop Experience)" { $Edition = "ServerStandard" }
+                "Windows Server 2019 Datacenter" { $Edition = "ServerDataCenterCore" }
+                "Windows Server 2019 Datacenter (Desktop Experience)" { $Edition = "ServerDataCenter" }
+                Default {$Edition = $Image.ImageName}
+            }
+
+            Write-Verbose "Edition: $Edition"
+            # Replace spaces with hyphens
+            $VHDName = $Image.ImageName -replace " ", "-"
+
+            # Remove unwanted characters
+            $VHDName = $VHDName -replace $pattern, ''
+
+            # Add Extension
+            $VHDName = "$VHDName.vhdx"
+            Write-Verbose "VHDName: $($VHDName)"
+            $VHDOutputPath = "$OutputDirectory$VHDName"
+            Write-Verbose "VHDOuput: $($VHDOutputPath)"
+            
+            Convert-WindowsImage -WIM $WIM.FullName -DiskType Dynamic -Size 50GB -VHDFormat VHDX -VHDPath $VHDOutputPath -Edition $Edition
+        }
+    }
+    
+    end {
+        
+    }
+}
